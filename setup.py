1fee8e2325b1dc2872482319df707bad7f91bcbf
shenyu <459240868@qq.com>
2019-12-03 14:21:15 +0800

    update

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as fh:
-    requirements = fh.read()
-requirements = requirements.split('\n')
-requirements = [requirement.strip() for requirement in requirements]
+requirements = ['numpy==1.16.4',
+'six==1.11.0',
+'scipy==1.3.0',
+'pandas==0.23.0',
+'scikit-learn==0.21.3',
+'tqdm==4.34.0',
+'hyperopt==0.1.2',
+'ConfigSpace==0.4.11',
+'smac==0.11.1']
 
 setup(name='alphaml',
       version='0.1.0',
